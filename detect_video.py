6ccf7d86ed1ccda659253d1e52cdb5e58a5a3548
Matthew Thomas <thomasmatt@me.com>
2020-10-30 20:06:05 -0700

    added args to detect_video.py

--- detect_video.py
+++ detect_video.py
@@ -6,30 +6,38 @@ import cv2
 import ssl
 from inference import infer_image
 import time
+from absl import app, flags
 
-ssl._create_default_https_context = ssl._create_unverified_context
-#model = hub.load("https://tfhub.dev/tensorflow/ssd_mobilenet_v2/2")
-model = hub.load("https://tfhub.dev/tensorflow/centernet/resnet50v2_512x512/1")
+FLAGS = flags.FLAGS
 
-cap = cv2.VideoCapture(0)
+flags.DEFINE_string('url', 'https://tfhub.dev/tensorflow/ssd_mobilenet_v2/2', 'hub url for model or path to model')
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    image_np = frame
-    
-    # infer frame
-    start_time = time.time()
-    result = infer_image(image_np, model)
-    fps = 1.0 / (time.time() - start_time)
-    print("FPS: %.2f" % fps)
+def main(argv):
+    ssl._create_default_https_context = ssl._create_unverified_context
+    model = hub.load(FLAGS.url)
 
-    # Display the resulting frame
-    cv2.imshow("result", result)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    cap = cv2.VideoCapture(0)
+
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        image_np = frame
+        
+        # infer frame
+        start_time = time.time()
+        result = infer_image(image_np, model)
+        fps = 1.0 / (time.time() - start_time)
+        print("FPS: %.2f" % fps)
+
+        # Display the resulting frame
+        cv2.imshow("result", result)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    app.run(main)
