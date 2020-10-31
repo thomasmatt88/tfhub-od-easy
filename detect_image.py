f23f26c31bbaf74b85ce011c845dc049e8c8368e
Matthew Thomas <thomasmatt@me.com>
2020-10-30 19:59:40 -0700

    added args to detect_image.py

--- detect_image.py
+++ detect_image.py
@@ -5,25 +5,29 @@ import numpy as np
 from PIL import Image
 import ssl
 from inference import infer_image
+from absl import app, flags
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('url', 'https://tfhub.dev/tensorflow/ssd_mobilenet_v2/2', 'hub url for model or path to model')
+flags.DEFINE_string('image_input', './data/kite.jpg', 'path to image')
+flags.DEFINE_string('image_output', '.detect-test.jpg', 'path to image')
 
 
-image_np = np.array(Image.open('./data/kite.jpg'))
+def main(argv):
+    image_np = np.array(Image.open(FLAGS.image_input))
 
-ssl._create_default_https_context = ssl._create_unverified_context
-#model = hub.load("https://tfhub.dev/tensorflow/faster_rcnn/inception_resnet_v2_640x640/1")
-#model = hub.load("https://tfhub.dev/tensorflow/ssd_mobilenet_v2/2")
-#model = hub.load('saved_models/ssd_mobilenet_v2_2')
-model = hub.load("https://tfhub.dev/tensorflow/centernet/resnet50v2_512x512/1")
-#model = hub.load("https://tfhub.dev/tensorflow/efficientdet/d7/1")
-#model = hub.load("https://tfhub.dev/tensorflow/retinanet/resnet50_v1_fpn_640x640/1")
-#model = hub.load("https://tfhub.dev/tensorflow/centernet/resnet50v2_512x512/1")
-#model = hub.load("https://tfhub.dev/tensorflow/centernet/hourglass_1024x1024_kpts/1")
+    ssl._create_default_https_context = ssl._create_unverified_context
+    model = hub.load(FLAGS.url)
 
-# Check the model's input signature
-print("input signature", model.signatures['serving_default'].inputs[0])
-# Tensor("input_tensor:0", shape=(1, None, None, 3), dtype=uint8)
+    # Check the model's input signature
+    print("input signature", model.signatures['serving_default'].inputs[0])
+    # Tensor("input_tensor:0", shape=(1, None, None, 3), dtype=uint8)
 
-# Run inference
-image_np = infer_image(image_np, model)
+    # Run inference
+    image_np = infer_image(image_np, model)
 
-Image.fromarray(image_np).save('detect-test.jpg')
+    Image.fromarray(image_np).save(FLAGS.image_output)
+
+if __name__ == '__main__':
+    app.run(main)
